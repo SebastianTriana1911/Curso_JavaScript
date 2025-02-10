a6deb261ed453402937a7b4641966a856abd0b04
Sebastian <sebastiantriana1911@gmail.com>
2025-02-10 14:27:50 -0500

    End of module Operators'

--- 3. Assignment operator/js/logicalOperator.js
+++ 3. Assignment operator/js/logicalOperator.js
@@ -37,3 +37,48 @@ let c = 1;
 let d = 10;
 let e = 20;
 console.log(c || d || e); // If all are true, return the first value
+
+
+// Logical operator and (&&)
+let f = true;
+let g = false;
+console.log(f && f); // Return True
+console.log(f && g); // Return False 
+console.log(g && f); // Return False 
+console.log(g && g); // Return False 
+
+// AND operator with data types different of the boolean
+console.log(0 && undefined); // If both operator are false, return the value of first: 0
+console.log(null && 55); // Return first operator: null 
+console.log(12 && -1); // Return second operator: -1
+console.log(20 && 25); // If both operator are true, return the first value: 25
+
+// Multiple values applied to the AND operator
+console.log(1 && 2 && null); // Return the first value false: null
+console.log(0 && -1 && undefined); // Return the first value false: 0
+console.log(1 && 2 && 3); // Return the last value true: 3
+
+// Logical operator not (!)
+// The not operator works when you want to deny something, if you have a variable with a true value when you deny it its value becomes false
+let h = 100;
+let i = 50;
+console.log(!(h == i)); // True
+
+
+// Logical operator Ternario
+// Sintaxis: condicionEvaluar ? expresion1(Si es verdadero):expresion2(Si es falso)
+let age = 18;
+let assess = (age >= 18) ? "You are of legal age" : "You are not legal age";
+console.log(assess)
+
+
+// Operator optional chaining (Encadenamiento opcional)
+// This operator works with the syntax "?.", it is used to call properties of an object, in order to not break the code when calling an attribute which has a null or undefined value, otherwise it will show that the value of that attribute was either null or undefined.
+
+const person = {
+    namePerson : "Sebas",
+    agePerson : null,
+    address : "Soacha"
+};
+const agePerson = person?.agePerson;
+console.log(agePerson);
