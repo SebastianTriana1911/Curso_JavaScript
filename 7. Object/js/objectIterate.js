d8cd6b16920b4619f4851e3af680a73585705257
Sebastian <sebastiantriana1911@gmail.com>
2025-03-20 18:15:10 -0500

    Iterate of object with method

---
+++ 7. Object/js/objectIterate.js
@@ -0,0 +1,18 @@
+// ITERATE OBJECT
+
+const tablet = {
+    reference: 12345,
+    dimensions: "10.2 inches",
+    memory: "64GB",
+    price: 300, 
+};
+
+// Object.keys(): Return a array with the names od the properties from object.
+console.log(Object.keys(tablet));
+
+// Object.values(): Return a array with the values corresponding to the properties from object.
+console.log(Object.values(tablet));
+
+// Object.entries(): Return a array of the keys and value of the given object
+console.log(Object.entries(tablet));
+
