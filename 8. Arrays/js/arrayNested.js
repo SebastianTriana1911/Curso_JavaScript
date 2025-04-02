b2ef428ba15ed77ca4173982510e578085e087bc
Sebastian <sebastiantriana1911@gmail.com>
2025-04-02 16:24:32 -0500

    Arrays Nested

---
+++ 8. Arrays/js/arrayNested.js
@@ -0,0 +1,10 @@
+// ARRAY NESTED
+
+const shoppingCart = ["Tablet", "Monitore", "Laptop", "Desktop", ["Processors", "Mother board", "Memorys",],];
+console.log(shoppingCart);
+
+// Accessing the array nested
+console.log(shoppingCart[4]); // By accessing index "4" we are accessing the nested array
+
+// Accessing an element from a nested array
+console.log(shoppingCart[4][0])
